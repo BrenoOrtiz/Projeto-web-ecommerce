76085f052622ea7bf63017ed18eb46effa3eed0e
Breno <breno.ortizdavid@outlook.com>
2023-10-18 22:29:03 -0300

    changes in index
    Search Bar implementation and css changes

--- usermain/php/searchProdutos.php
+++ usermain/php/searchProdutos.php
@@ -1,13 +1,17 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', 'Bod12345:)Breno', 'ecommerce');  
 
-if (!$connection){
+$busca = $_POST["search"];
+
+$conn = mysqli_connect('localhost', 'root', 'Bod12345:)Breno', 'ecommerce');
+
+if (!$conn) {
     echo "Connection error: " . mysqli_connect_error();
     exit();
 }
-$query = 'SELECT * FROM produtos';
 
-$result = mysqli_query($connection, $query);
+$query = "SELECT * FROM produtos WHERE nome LIKE '%$busca%' ";
+
+$result = mysqli_query($conn, $query);
 $data = array();
 
 while($registro = mysqli_fetch_assoc($result)){
